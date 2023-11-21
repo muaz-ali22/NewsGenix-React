282dab885edabd1132a7a8df681357d4666834f8
Muaz <muazalikhan2000@gmail.com>
2023-11-21 05:48:25 +0500

    Add some logic and add spinner component

--- src/Components/NewsComponent.js
+++ src/Components/NewsComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItems from "./NewsItems";
+import Spinner from "./Spinner";
 
 export default class NewsComponent extends Component {
   constructor() {
@@ -11,42 +12,49 @@ export default class NewsComponent extends Component {
     };
   }
   handleNext = async () => {
-    let Url = `https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=8e04e5a4b90b471ca1ba43da8c1c393a&pageSize=9&page=${
-      this.state.page + 1
-    }`;
+    let Url = `https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=8e04e5a4b90b471ca1ba43da8c1c393a&pageSize=${
+      this.props.pageSize
+    }&page=${this.state.page + 1}`;
+    this.setState({ loading: true });
     let data = await fetch(Url);
     // console.log(data);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       page: this.state.page + 1,
+      loading: false,
       articles: parsedData.articles,
     });
   };
 
   handlePrevious = async () => {
-    let Url = `https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=8e04e5a4b90b471ca1ba43da8c1c393a&pageSize=9&page=${
-      this.state.page - 1
-    }`;
+    let Url = `https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=8e04e5a4b90b471ca1ba43da8c1c393a&pageSize=${
+      this.props.pageSize
+    }&page=${this.state.page - 1}`;
+    this.setState({ loading: true });
+
     let data = await fetch(Url);
     // console.log(data);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       page: this.state.page - 1,
+      loading: false,
       articles: parsedData.articles,
     });
   };
   async componentDidMount() {
-    let Url =
-      "https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=8e04e5a4b90b471ca1ba43da8c1c393a&pageSize=9";
+    let Url = `https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=8e04e5a4b90b471ca1ba43da8c1c393a&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+
     let data = await fetch(Url);
+
     // console.log(data);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
-      pageSize: (this.state.pageSize = 9),
+      loading: false,
       totalResults: parsedData.totalResults,
     });
   }
@@ -54,6 +62,7 @@ export default class NewsComponent extends Component {
     return (
       <div className="container my-3 ">
         <h1
+          className="text-center"
           style={{
             border: "2px solid rgb(155,29,33)",
             backgroundColor: "rgb(155,29,33)",
@@ -61,34 +70,38 @@ export default class NewsComponent extends Component {
             color: "wheat",
           }}
         >
-          NewsGenix - Top Headlines
+          NewsGenix - {this.props.NewsType}
         </h1>
+        {this.state.loading && <Spinner />}
 
-        <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItems
-                  title={element.title ? element.title.slice(0, 35) : ""}
-                  description={
-                    element.description ? element.description.slice(0, 75) : ""
-                  }
-                  imageUrl={
-                    element.urlToImage
-                      ? element.urlToImage
-                      : "https://thumbs.dreamstime.com/z/world-map-technological-background-best-internet-concept-global-business-elements-image-furnished-bright-lines-66529240.jpg?w=992"
-                  }
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+        <div className="row ">
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4 text-center" key={element.url}>
+                  <NewsItems
+                    title={element.title ? element.title.slice(0, 35) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 75)
+                        : ""
+                    }
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://thumbs.dreamstime.com/z/world-map-technological-background-best-internet-concept-global-business-elements-image-furnished-bright-lines-66529240.jpg?w=992"
+                    }
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container my-3 d-flex justify-content-between">
           <button
             type="button"
             disabled={this.state.page <= 1}
-            class="btn btn-primary"
+            className="btn btn-primary"
             onClick={this.handlePrevious}
           >
             &larr; Previous
@@ -96,10 +109,10 @@ export default class NewsComponent extends Component {
           <button
             disabled={
               this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.state.pageSize)
+              Math.ceil(this.state.totalResults / this.props.pageSize)
             }
             type="button"
-            class="btn btn-primary"
+            className="btn btn-primary"
             onClick={this.handleNext}
           >
             Next &rarr;
